3b1c5ceccbe581572fda82bcccd6db301d0574ec
Rafael <rafaelbitencourtn12@gmail.com>
2021-07-20 15:21:12 -0300

    ok

--- scrip.js
+++ scrip.js
@@ -58,7 +58,32 @@ function prevSong() {
   playSong();
 }
 
-function nextSong() {}
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function upDateProgress(e) {
+  /* console.log(e.srcElement.duration); */
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
 
 ///Event listeners
 
@@ -76,3 +101,11 @@ playBtn.addEventListener("click", () => {
 
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+
+//avançar musica
+
+audio.addEventListener("timeupdate", upDateProgress);
+progressContaniner.addEventListener("click", setProgress);
+
+//tocar a proxima musica
+audio.addEventListener("ended", nextSong);
